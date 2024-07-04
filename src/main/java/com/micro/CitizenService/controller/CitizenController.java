c4265dd754051987b533347a25e703fa9cc777f6
Nayan Singh <nayansingh196@gmail.com>
2024-07-05 02:25:56 +0530

    Add Itentity Provider and and Security

--- src/main/java/com/micro/CitizenService/controller/CitizenController.java
+++ src/main/java/com/micro/CitizenService/controller/CitizenController.java
@@ -2,9 +2,13 @@ package com.micro.CitizenService.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,25 +21,38 @@ import com.micro.CitizenService.repository.CitizenRepo;
 @RestController
 @RequestMapping("/citizen")
 public class CitizenController {
-	
+
+	Logger LOG = LoggerFactory.getLogger(CitizenController.class);
+
 	@Autowired
 	CitizenRepo repo;
 	
+	@Autowired
+	Environment environment;
+
 	@RequestMapping("/test")
 	public ResponseEntity<String> test() {
-		return new ResponseEntity<>("Hello",HttpStatus.OK);
+		return new ResponseEntity<>("Hello", HttpStatus.OK);
 	}
-	
+
+	@PreAuthorize("hasAuthority('SCOPE_internal')")
 	@RequestMapping("/id/{id}")
 	public ResponseEntity<List<Citizen>> getById(@PathVariable Integer id) {
-		List<Citizen> citizenList=repo.findByVaccinationCenterId(id);
-		return new ResponseEntity<>(citizenList,HttpStatus.OK);
+		String port = environment.getProperty("local.server.port");
+		LOG.info("Citizen Instance port {}", port);
+		List<Citizen> citizenList = repo.findByVaccinationCenterId(id);
+		if (citizenList.isEmpty()) {
+			LOG.info("No Citizen Available for Vaccination Center {}", id);
+		} else {
+			LOG.info("Citizenz Available for Vaccination Center {}", id);
+		}
+		return new ResponseEntity<>(citizenList, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/add")
 	public ResponseEntity<Citizen> addCitizen(@RequestBody Citizen newCitizen) {
-		Citizen citizen=repo.save(newCitizen);
-		return new ResponseEntity<>(citizen,HttpStatus.OK);
+		Citizen citizen = repo.save(newCitizen);
+		return new ResponseEntity<>(citizen, HttpStatus.OK);
 	}
 
 }
